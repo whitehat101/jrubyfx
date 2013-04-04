7343819803acfc2e59a73ee819947df7bbbb4f45
Jeremy Ebler <jebler@gmail.com>
2013-04-03 19:39:53 -0700

    with property discovery

--- lib/jrubyfx/utils/inspector.rb
+++ lib/jrubyfx/utils/inspector.rb
@@ -15,12 +15,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 =end
 module JRubyFX::Utils::Inspector
+
   class << self
     def included(base)
       base.extend ClassMethods
-      base.inspect_properties
     end
   end
+
   module ClassMethods
     def inspect_properties props = {}
       @inspect_properties = props
@@ -29,15 +30,29 @@ module JRubyFX::Utils::Inspector
 
   def inspect
     props = ["#{self.class}:#{'0x%08x' % object_id}"]
-    props += (self.class.instance_variable_get('@inspect_properties')||{}).map do |key,val|
-      if val.is_a? Array
-        format = val.shift
-        val.map! {|v|send v}
-        "#{key}:#{sprintf format, *val}"
-      else
-        "#{key}:#{send val}"
+    props += (self.class.instance_variable_get('@inspect_properties')||discover_properties).
+      map do |key,val|
+        if val.is_a? Array
+          format = val.shift
+          val.map! {|v|send v}
+          "#{key}:#{sprintf format, *val}"
+        else
+          "#{key}:#{send val}"
+        end
       end
-    end
     "#<#{props.join ' '}>"
   end
+
+private
+
+  # Use reflection to discover properties
+  def discover_properties
+    props = {}
+    (java_class.java_instance_methods - java_class.superclass.java_instance_methods).
+      select {|m|m.return_type and m.return_type.interfaces.include? Java::JavafxBeansProperty::Property.java_class}.
+      map    {|m|send(m.name).getName}.
+      each   {|p|props.merge! p => p}
+    self.class.instance_variable_set '@inspect_properties', props
+  end
+
 end
